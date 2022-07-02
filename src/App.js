8ce554a3627c489697143df66958be97b196547d
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2022-07-02 10:58:09 +0600

    login singup resetpassword 404 page done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Singup from './Pages/Singup/Singup';
+import ForgetPassword from './Pages/ForgetPassword/ForgetPassword';
+import NotFoundPage from './Pages/NotFoundPage/NotFoundPage';
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/singup' element={<Singup></Singup>}></Route>
+        <Route path='/forgetpassword' element={<ForgetPassword></ForgetPassword>}></Route>
+        <Route path='*' element={<NotFoundPage></NotFoundPage>}></Route>
       </Routes>
       <Footer />
     </div>
